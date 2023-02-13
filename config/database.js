c84ee6fbef3082ccbf7890c3323ad3db54cad438
EffiEmmanuel <effiemmanuel.n#gmail.com>
2023-02-13 11:52:24 +0100

    Added appointments

--- config/database.js
+++ config/database.js
@@ -1,11 +1,18 @@
-const path = require('path');
-
+// strapi-api/config/database.js
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "internistika"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "effi"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      //   ssl: {
+      //     rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      //   },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
